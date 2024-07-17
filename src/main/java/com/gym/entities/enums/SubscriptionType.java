bcf202845f53cee451fdf9931f7de450e209b6d5
Nikita Savvin <nik.savvin.2004@mail.ru>
2024-07-17 12:10:23 +0300

    update SubscriptionType

--- src/main/java/com/gym/entities/enums/SubscriptionType.java
+++ src/main/java/com/gym/entities/enums/SubscriptionType.java
@@ -1,24 +1,31 @@
 package com.gym.entities.enums;
 
 public enum SubscriptionType {
-    PREMIUM("Private"),
-    BASIC("Basic"),
-    GROUP("Group");
+    PREMIUM("Premium", 25000),
+    BASIC("Basic", 15000),
+    GROUP("Group", 19900);
 
     private final String title;
+    private final int costMonth;
 
-    SubscriptionType(String title){
+    SubscriptionType(String title, int costMonth){
         this.title = title;
+        this.costMonth = costMonth;
     }
 
     private String getTitle(){
         return title;
     }
 
+    public int getCostMonth() {
+        return costMonth;
+    }
+
     @Override
     public String toString() {
         return "SubscriptionType{" +
                 "title='" + title + '\'' +
+                ", costMonth=" + costMonth +
                 '}';
     }
 }
