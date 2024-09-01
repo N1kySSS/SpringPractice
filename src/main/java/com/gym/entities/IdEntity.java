ed640cb8961e9680af1cedc837fdba135a6a8f79
Nikita Savvin <nik.savvin.2004@mail.ru>
2024-09-01 20:31:03 +0300

    add annotation @Column(name = 'id'

--- src/main/java/com/gym/entities/IdEntity.java
+++ src/main/java/com/gym/entities/IdEntity.java
@@ -1,16 +1,13 @@
 package com.gym.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
 @MappedSuperclass
 public abstract class IdEntity {
-
     private Long id;
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
         return id;
