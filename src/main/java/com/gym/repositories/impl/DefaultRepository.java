ab5066d362446a4e72e40638b4a4c07cc0d9fef4
Nikita Savvin <nik.savvin.2004@mail.ru>
2024-09-08 15:15:56 +0300

    fix

--- src/main/java/com/gym/repositories/impl/DefaultRepository.java
+++ src/main/java/com/gym/repositories/impl/DefaultRepository.java
@@ -1,9 +1,8 @@
 package com.gym.repositories.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.Repository;
 
-public abstract class DefaultRepository<T extends Repository<?, ?>> {
+public abstract class DefaultRepository<T> {
 
     @Autowired
     protected T defaultRepository;
